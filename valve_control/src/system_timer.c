d91a75dfe4ed116241875df2197d9f817df281c4
Kenneth Thompson <>
2020-02-12 22:36:16 -0800

    valve_control: Set system timer initial value to 0

--- valve_control/src/system_timer.c
+++ valve_control/src/system_timer.c
@@ -7,12 +7,13 @@ uint32_t systick_ms_count;
 void init_system_timer(void)
 {
     /* initialize the millisecond timer */
-    SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;  /* enable systick interrupt */
+    SysTick->CTRL |= SysTick_CTRL_TICKINT_Msk;  /* enable SysTick interrupt */
     SysTick->CTRL &= ~SysTick_CTRL_CLKSOURCE_Msk;  /* use SystemCoreClock / 8 */
     const uint32_t reload_value = SystemCoreClock / 8 / 1000 - 1;  /* 1000 Hz */
     SysTick->LOAD = reload_value;
     systick_ms_count = 0;
-    SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;  /* enable the systick timer */
+    SysTick->VAL = 0;  /* reset SysTick counter to zero */
+    SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;  /* enable the SysTick timer */
 }
 
 void SysTick_Handler(void)
