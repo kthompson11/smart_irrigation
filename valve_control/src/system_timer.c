cf6e100bd4a459028f72f124a6f2002b83eafb05
Kenneth Thompson <>
2020-02-19 15:10:19 -0800

    valve_control: Change ms count to 64-bit struct
    In order to provide a monotonically increasing value for milliseconds
    since boot, system time is now represented by a 64-bit struct containing
    an upper and lower 32-bit integer.

--- valve_control/src/system_timer.c
+++ valve_control/src/system_timer.c
@@ -2,7 +2,7 @@
 
 #include "system_timer.h"
 
-uint32_t systick_ms_count;
+static struct systick_ms ms_count;
 
 void init_system_timer(void)
 {
@@ -11,17 +11,54 @@ void init_system_timer(void)
     SysTick->CTRL &= ~SysTick_CTRL_CLKSOURCE_Msk;  /* use SystemCoreClock / 8 */
     const uint32_t reload_value = SystemCoreClock / 8 / 1000 - 1;  /* 1000 Hz */
     SysTick->LOAD = reload_value;
-    systick_ms_count = 0;
+    ms_count.lower = 0;
+    ms_count.upper = 0;
     SysTick->VAL = 0;  /* reset SysTick counter to zero */
     SysTick->CTRL |= SysTick_CTRL_ENABLE_Msk;  /* enable the SysTick timer */
 }
 
-uint32_t get_systick_ms_count(void)
+struct systick_ms get_systick_ms_count(void)
 {
-    return systick_ms_count;
+    return ms_count;
+}
+
+/* Compare two systick_ms_count structs and returns 1 if the first struct
+ * represents a later time than the second struct */
+int systick_gt(struct systick_ms lhs, struct systick_ms rhs)
+{
+    int res = 0;
+
+    if (lhs.upper > rhs.upper)
+    {
+        res = 1;
+    }
+    else if ((lhs.upper == rhs.upper) && (lhs.lower > rhs.lower))
+    {
+        res = 1;
+    }
+    
+    return res;
+}
+
+struct systick_ms systick_ms_add(struct systick_ms lhs, uint32_t rhs)
+{
+    struct systick_ms res;
+    res.lower = lhs.lower + rhs;
+
+    /* check for overflow of lower counter */
+    if (lhs.lower > res.lower)
+    {
+        res.upper = lhs.upper + 1;
+    }
+    else
+    {
+        res.upper = lhs.upper;
+    }
+    
+    return res;
 }
 
 void SysTick_Handler(void)
 {
-    systick_ms_count += 1;
+    ms_count = systick_ms_add(ms_count, 1);
 }
--- valve_control/src/system_timer.h
+++ valve_control/src/system_timer.h
@@ -3,7 +3,17 @@
 
 #include <stdint.h>
 
+/* describes a 64 bit counter representing time elapsed 
+ * (in milliseconds) from timer initialization */
+struct systick_ms 
+{
+    uint32_t lower;
+    uint32_t upper;
+};
+
 void init_system_timer(void);
-uint32_t get_systick_ms_count(void);
+struct systick_ms get_systick_ms_count(void);
+int systick_gt(struct systick_ms lhs, struct systick_ms rhs);
+struct systick_ms systick_ms_add(struct systick_ms lhs, uint32_t rhs);
 
 #endif  /* SYSTEM_TIMER_H */
