f7ec973def9162de6798107935781d8ccc1e56d7
Kenneth Thompson <>
2020-04-20 16:59:06 -0700

    meta-irr_control: Add probe and remove to valve-control

--- timer_control/yocto/meta-irr_control/recipes-irr/valve-control/valve-control/valve-control.c
+++ timer_control/yocto/meta-irr_control/recipes-irr/valve-control/valve-control/valve-control.c
@@ -1,19 +1,28 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/spi/spi.h>
+#include <linux/cdev.h>
 
 #define N_MINORS 1
 
-struct valve_control_data {
+/* device data */
+struct device_data {
 	dev_t devt;
+	struct cdev cdev;
 	spinlock_t lock;
 	struct spi_device *spi;
 	unsigned users;
 };
-/* for now, put all data in one struct because only one device is supported */
-static struct valve_control_data drv_data;
+/* For now, only one device is supported, so 
+   store the device data as a global variable. */
+static struct device_data *dev_data;
 
-static struct class *valve_control_class;
+/* global driver data */
+struct driver_data {
+	dev_t devt;
+	struct class *class;
+};
+static struct driver_data drv_data;
 
 /************************* file_operations functions **************************/
 
@@ -52,11 +61,65 @@ static const struct file_operations valve_control_fops = {
 
 static int valve_control_probe(struct spi_device *spi)
 {
+	int status;
+	struct device_data *data;
+	struct device *dev;
+
+	/* check if a device is already handled by the driver */
+	if (dev_data != NULL)
+		return -ENODEV;
+
+	data = kzalloc(sizeof(*data), GFP_KERNEL);
+	if (!data)
+		return -ENOMEM;
+
+	/* initialize device data fields */
+	data->devt = drv_data.devt; /* only one device */
+	spin_lock_init(&data->lock);
+	data->spi = spi;
+	cdev_init(&data->cdev, &valve_control_fops);
+	data->cdev.owner = THIS_MODULE;
+
+	/* set spi deviver_data with the device's data */
+	spi_set_drvdata(spi, data);
+
+	/* store the device_data globally */
+	dev_data = data;
+
+	/* add device to system */
+	status = cdev_add(&data->cdev, data->devt, 1);
+	if (status)
+		goto err_cdev_add;
+
+	/* create device file */
+	dev = device_create(drv_data.class, &spi->dev, data->devt, 
+			    data, "valve-control");
+	if (IS_ERR(dev)) {
+		status = PTR_ERR(dev);
+		goto err_device_create;
+	}
+
 	return 0;
+
+err_device_create:
+	cdev_del(&data->cdev);
+err_cdev_add:
+	kfree(data);
+	return status;
 }
 
 static int valve_control_remove(struct spi_device *spi)
 {
+	struct device_data *data = spi_get_drvdata(spi);
+
+	/* prevent further file operations */
+	spin_lock_irq(&data->lock);
+	data->spi = NULL;
+	spin_unlock_irq(&data->lock);
+
+	device_destroy(drv_data.class, data->devt);
+	cdev_del(&data->cdev);
+	kfree(data);
 	return 0;
 }
 
@@ -85,9 +148,9 @@ static int __init valve_control_init(void)
 		return status;
 	}
 
-	valve_control_class = class_create(THIS_MODULE, "valve-control");
-	if (IS_ERR(valve_control_class)) {
-		status = PTR_ERR(valve_control_class);
+	drv_data.class = class_create(THIS_MODULE, "valve-control");
+	if (IS_ERR(drv_data.class)) {
+		status = PTR_ERR(drv_data.class);
 		goto err_create_class;
 	}
 
@@ -99,7 +162,7 @@ static int __init valve_control_init(void)
 	return status;
 
 err_spi_register:
-	class_destroy(valve_control_class);
+	class_destroy(drv_data.class);
 err_create_class:
 	unregister_chrdev_region(drv_data.devt, N_MINORS);
 	return status;
@@ -110,7 +173,7 @@ module_init(valve_control_init);
 static void __exit valve_control_exit(void)
 {
 	spi_unregister_driver(&valve_control_driver);
-	class_destroy(valve_control_class);
+	class_destroy(drv_data.class);
 	unregister_chrdev_region(drv_data.devt, N_MINORS);
 }
 
