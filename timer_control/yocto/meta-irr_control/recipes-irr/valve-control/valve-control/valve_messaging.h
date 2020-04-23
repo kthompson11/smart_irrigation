26606c55587d973fba6598102b783e9e97ca4bf7
Kenneth Thompson <>
2020-04-23 13:11:17 -0700

    meta-irr_control: Modify valve-control interface file

--- timer_control/yocto/meta-irr_control/recipes-irr/valve-control/valve-control/valve_messaging.h
+++ timer_control/yocto/meta-irr_control/recipes-irr/valve-control/valve-control/valve_messaging.h
@@ -1,19 +1,40 @@
-/* Driver-side header for the driver-microcontroller interface. */
+/* driver-microcontroller interface header */
 
 #ifndef VALVE_MESSAGING_H
 #define VALVE_MESSAGING_H
 
+#ifdef __KERNEL__
+    #define uint8_t u8
+#else
+#include <stdint.h>
+#endif /* __KERNEL__
+
 /* 
- * valve_control message format
+ * valve_control request format
  *  |---- arg (5 bits) ----|---- opcode (3 bits) ----|
- * lsb                                              msb
+ * msb                                              lsb
  */
 #define VALVE_OPCODE_LEN 3
 #define VALVE_ARG_LEN 5
+#define VALVE_OPCODE_MASK 0x07
 
 #define VALVE_OPCODE_OPEN  0x00
 #define VALVE_OPCODE_CLOSE 0x01
 
-#define VC_MAKE_OP(opcode, arg) ((arg << VALVE_OPCODE_LEN) | opcode)
+
+static inline uint8_t vc_make_request(opcode, arg)
+{
+    return ((arg << VALVE_OPCODE_LEN) | opcode);
+}
+
+static inline uint8_t vc_get_opcode(request)
+{
+    return (request >> VALVE_OPCODE_LEN);
+}
+
+static inline uint8_t vc_get_arg(request)
+{
+    return (request & VALVE_OPCODE_MASK);
+}
 
 #endif  /* VALVE_MESSAGING_H */
