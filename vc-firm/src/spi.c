6591a159c76d78e6261198bd6d1e80b333f8f44f
Kenneth Thompson <>
2020-05-12 15:32:37 -0700

    vc-firm: Sync SPI data frame using interrupt

--- vc-firm/src/spi.c
+++ vc-firm/src/spi.c
@@ -6,6 +6,15 @@
 #include "spi.h"
 #include "vc-interface.h"
 
+static int bytes_received = 0;
+
+/**
+ * Synchronizes the SPI data frame
+ */
+void start_spi_data_frame(void) {
+    bytes_received = 0;
+}
+
 void spi1_init(void)
 {
     /* enable SPI1 clock */
@@ -38,6 +47,14 @@ void spi1_init(void)
 
     /* enable SPI1 */
     SET_BIT(SPI1->CR1, SPI_CR1_SPE);
+
+    /* enable interrupts on CS pin */
+    /* set NVIC registers */
+    NVIC_EnableIRQ(7);
+    /* set EXTI registers */
+    SET_BIT(EXTI->IMR, EXTI_IMR_IM4);  /* enable external interrupt 4 */
+    SET_BIT(EXTI->FTSR, EXTI_FTSR_FT4);  /* set interrupt for falling edge */
+    MODIFY_REG(SYSCFG->EXTICR[2], SYSCFG_EXTICR2_EXTI4_Msk, SYSCFG_EXTICR2_EXTI4_PA);
 }
 
 void spi_task(void *param)
@@ -46,20 +63,11 @@ void spi_task(void *param)
     req_type response, request;
     const int rx_size = sizeof(req_type);
     uint8_t rx_buf[rx_size];
-    int bytes_received = 0;
-    int prev_cs = GPIOA->IDR & GPIO_IDR_4;
 
     /* continuously poll for incoming requests */
     /* TODO: switch from polling to interrupts */
     for (;;) {
         /******************** handle rx data ********************/
-        /* reset bytes_received on assertion (falling edge) of chip select */
-        int current_cs = GPIOA->IDR & GPIO_IDR_4;
-        if ((prev_cs != current_cs) && (current_cs == 0)) {
-            bytes_received = 0;
-            prev_cs = current_cs;
-        }
-
         int is_rx_pending = SPI1->SR & SPI_SR_RXNE;
         if (is_rx_pending) {
                 rx_buf[bytes_received] = SPI1->DR;
@@ -78,5 +86,17 @@ void spi_task(void *param)
         /* discard response data for now */
         /* TODO: respond to request */
         xQueueReceive(data->miso_queue, &response, 0);
+    }
+}
+
+void EXTI4_15_IRQHandler(void)
+{
+    /* get pending interrupt status */
+    uint32_t pr = EXTI->PR;
+
+    /* determine cause of interrupt */
+    if (pr & EXTI_PR_PIF4) {
+        start_spi_data_frame();
+        SET_BIT(EXTI->PR, EXTI_PR_PIF4);
     }
 }
